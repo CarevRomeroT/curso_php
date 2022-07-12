a507c5f59a3efcfc031b0980186b4343eeca339e
CarevRomeroT <carevromero@colegioguanenta.edu.co>
2022-07-12 09:59:27 -0500

    Ejercicios practica

--- practica_php.php
+++ practica_php.php
@@ -50,4 +50,19 @@
     echo $cambiodelinea;
     $z = 'buenas ';
     $z.= 'noches';
+   
+    echo $z;
+    $numeros[0] = 1;
+    $numeros[1] = 2;
+    $numeros[2] = 3;
+    $numeros[3] = 4;
+
+    echo $numeros[3];
+    echo "hola";
+
+
+
+    echo "sofi";
+
+
 ?>
