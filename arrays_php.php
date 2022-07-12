311950d5c7d5dad50e7b6a54dd71d41d83115700
CarevRomeroT <carevromero@colegioguanenta.edu.co>
2022-07-12 10:14:29 -0500

    Ejercicios practica

---
+++ arrays_php.php
@@ -0,0 +1,34 @@
+<?php
+//**ARRAYS */
+$cambiodelinea = "<br>";
+$numeros[0] = 1;
+$numeros[1] = 2;
+$numeros[2] = 3;
+$numeros[3] = 4;
+
+
+$letras[0] = 'uno';
+$letras[1] = 'dos';
+$letras[2] = 'tres';
+$letras[3] = 'cuatro';
+
+echo $numeros[3];
+echo $cambiodelinea;
+echo $letras[1];
+echo $cambiodelinea;
+
+$numero = array(1,2,3,4,5);
+
+echo $numero[4]
+
+//ARRAYS ASOCIATIVAS
+$edad = array(
+    'juan' => 11,
+    'pedro' => 20,
+    'david' => 35
+);
+
+echo $edad['pedro'];
+
+
+?>
