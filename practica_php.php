3cc5457cb953351fdd99ecafd215e298dd1146c3
CarevRomeroT <carevromero@colegioguanenta.edu.co>
2022-07-12 09:01:26 -0500

    Ejercicios practica

---
+++ practica_php.php
@@ -0,0 +1,26 @@
+<?php 
+
+    echo 'Hola Carev<br>';
+
+    //* MANEJO DE VARIABLES*//
+
+    $nombre = "Lorena<br>";
+    echo $nombre;
+    $numeros = 12345;
+    echo $numeros;
+
+    //*CONSTANTES*//
+    define("codigo",3182595685);
+    echo codigo;
+
+    echo "<br>";
+
+    //*OPERADORES*//
+    $n = 0;
+    $nn = 2.6;
+    $nnn = true;
+    echo $n;
+
+    
+
+?>
